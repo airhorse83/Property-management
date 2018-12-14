25b3d0b799622dd9ed8a6c08b939670151616b12
airhorse83 <airhorse83@yahoo.com>
2018-12-13 17:22:44 -0700

    added redux form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SigninForm from './signinForm';
 
 class Signin extends Component {
     render() {
         return (
             <div className='sign-in'>
-                ...signin
+            
+                <SigninForm/>
             </div>
         )
     }
