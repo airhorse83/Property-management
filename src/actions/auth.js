c3c4cef1e8ca372d8b857c97c0f5848c0f126086
airhorse83 <airhorse83@yahoo.com>
2018-12-19 11:43:43 -0700

    handled signup post request'

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,13 +5,15 @@ import { ROOT_URL } from '../config';
 export function signUp(fields, success) {
     console.log(fields);
     return function(dispatch) {
-//         axios.post(`${ROOT_URL}/signUp`, fields)
-//         .then(response => {
+        axios.post(`${ROOT_URL}/signUp`, fields)
+        .then(response => {
+            console.log(response);
+            success()
+        })
+        .catch(err => {
+            if(err) { console.log(err) }
+        })
 
-//         })
-//         .catch(err => {
-//             if(err) { console.log(err) }
-//         })
-
-//     }
-// }
+    }
+}
+   
